18e325160e571911756fd8a6ae3ff1491dae1509
Andrew Adare <andrewadare@gmail.com>
2016-05-28 12:19:01 -0600

    Vehicle heading and steering angle measurements

--- firmware/steering/steering.ino
+++ firmware/steering/steering.ino
@@ -1,14 +1,43 @@
 
-// [stepper_and_lidar]$ arduino --board arduino:avr:uno --port /dev/ttyUSB0 --upload steering.ino -v
+// Compile and flash to Arduino Uno from command line (replace with your port):
+// arduino --board arduino:avr:uno --port /dev/ttyUSB0 --upload steering.ino -v
+
+#include "NAxisMotion.h"
+#include <Wire.h>
 
 #define STEP_PIN   3  // INT1/PD3/OC2B
 #define DIR_PIN    4  // Pin controlling rotation direction
-#define ENABLE_PIN 5  // Asserted low
-#define POT_PIN A0
+#define ENABLE_PIN 5  // Asserted low (connected to stepper driver)
+#define POT_PIN   A0  // Pin to read voltage from potentiometer
 
+// A4 and A5 are also used as the I2C data and clock lines for the BNO055 board.
+
+// Number of steps taken by motor after a command is issued.
 volatile unsigned long steps = 0;
-int incomingByte = 0; // for incoming serial data
-int angleADC = 0;
+
+// For incoming serial data
+byte incomingByte = 0;
+
+// Digitized reading from potentiometer
+unsigned int angleADC = 0;
+
+// Steering angle and limits. Orientation follows that of IMU sensor (positive
+// clockwise). Since map() uses integer math, the limits (+/- 30 deg) are
+// represented as integral units of 0.01 degree to limit loss of precision.
+int leftSteerMax = -3000;
+int rightSteerMax = 3000;
+float steerAngle = 0;
+
+// Steering angle limits as ADC readings. Empirically determined.
+int leftADC = 577;
+int rightADC = 254;
+
+// Timestamp of previous update step and update interval in ms
+unsigned long prevTime = 0;
+const unsigned long dt = 20;
+
+// Interface class instance for BNO055 sensor
+NAxisMotion imu;
 
 // Pin-change interrupt callback - increment step counter
 void count()
@@ -37,27 +66,42 @@ void setup()
 
   Serial.begin(115200);
 
+  // I2C and IMU sensor initialization
+  I2C.begin();
+  imu.initSensor();
+  imu.setOperationMode(OPERATION_MODE_NDOF);
+  imu.setUpdateMode(MANUAL);
+
+  // Set up count() as an interrupt handler
   attachInterrupt(digitalPinToInterrupt(STEP_PIN), count, FALLING);
 }
 
 void loop()
 {
 
-  steps = 0;
-  digitalWrite(DIR_PIN, LOW);
-  do {}
-  while (steps < 100);
+  if (millis() - prevTime >= dt)
+  {
+    prevTime = millis();
 
-  delay(500);
+    // Update Euler angle measurements and sensor calibration status
+    imu.updateEuler();
+    imu.updateCalibStatus();
 
-  steps = 0;
-  digitalWrite(DIR_PIN, HIGH);
-  do {}
-  while (steps < 100);
+    // Timestamp of current step
+    Serial.print(prevTime);
 
-  delay(500);
+    // Azimuthal bearing [deg]
+    Serial.print(" H: ");
+    Serial.print(imu.readEulerHeading());
 
-  angleADC = analogRead(POT_PIN);
-  Serial.println(angleADC);
+    // Digitized voltage drop on turnpot
+    Serial.print(" ADC: ");
+    angleADC = analogRead(POT_PIN);
+    Serial.print(angleADC);
 
+    // Computed steering angle [deg]
+    steerAngle = 0.01 * map(angleADC, leftADC, rightADC, leftSteerMax, rightSteerMax);
+    Serial.print(" ");
+    Serial.println(steerAngle);
+  }
 }
