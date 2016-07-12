4cddd530a33518f955e4f9ab62b0f4b7a16a0cb6
Andrew Adare <andrewadare@gmail.com>
2016-07-11 21:40:40 -0600

    Change steering firmware for new board (WIP)

--- firmware/steering/steering.ino
+++ firmware/steering/steering.ino
@@ -1,14 +1,9 @@
 // Arduino code for steering control unit
 
-#define STEP_PIN   3    // INT1/PD3/OC2B
-#define DIR_PIN    4    // Pin controlling rotation direction
-#define ENABLE_PIN 5    // Asserted low (connected to stepper driver)
+#define ENABLE_PIN  9   // Asserted low on stepper driver
+#define DIR_PIN    10   // Pin controlling rotation direction
+#define STEP_PIN   11   // INT1/PB3/OC2
 #define POT_PIN    A0   // Pin to read voltage from potentiometer
-
-#define PI                 3.14159265358979
-#define RAD_PER_DEG        PI/180
-#define MAX_STEERING_ANGLE PI/6  // Built-in steering limit of front wheels
-#define MAX_HEADING_CHANGE 0.281 // [rad] at max steering angle
 
 // Input command string
 String command;
@@ -19,23 +14,9 @@ volatile unsigned long steps = 0;
 // Digitized reading from potentiometer (running average - see ewma() below)
 unsigned int angleADC = 0;
 
-// Steering angle (positive clockwise) and limits.
-// Since map() uses integer math, the limits (+/-30 deg = pi/6) are
-// represented as integral units of 1e-4 rad to limit loss of precision.
-int leftSteerMax = -MAX_STEERING_ANGLE * 1e4;
-int rightSteerMax = MAX_STEERING_ANGLE * 1e4;
-float steerAngle = 0;
-
 // Steering angle limits as ADC readings. Empirically determined.
 int maxLeftADC = 577;
 int maxRightADC = 254;
-
-// Pin-change interrupt callback to increment step counter (only called when
-// PWM output is enabled).
-void count()
-{
-  steps++;
-}
 
 // Exponentially weighted moving average for integer data. Used for over-
 // sampling noisy measurements in a time series.
@@ -84,71 +65,66 @@ void steerTo(int targetADC, int tolerance)
 
 void handleCommands()
 {
-  if (Serial.available())
+  char c = Serial.read();
+  if (c == '\n')
   {
-    char c = Serial.read();
-    if (c == '\n')
+    String op = command;
+    int arg = 0;
+
+    // If command has a space, assign separate operation and argument parts
+    int spaceIndex = command.indexOf(" ");
+    if (spaceIndex >= 0)
     {
-      String op = command;
-      int arg = 0;
-
-      // If command has a space, assign separate operation and argument parts
-      int spaceIndex = command.indexOf(" ");
-      if (spaceIndex >= 0)
-      {
-        op = command.substring(0, spaceIndex);
-        arg = command.substring(spaceIndex + 1).toInt();
-      }
-
-      // Please reply to request for latest steering angle reading
-      if (op.equalsIgnoreCase("u"))
-      {
-        String s = String("adc:") + (angleADC >> 4) + String(",steps:") + steps;
-        Serial.println(s);
-      }
-
-      // Steer the car - expecting that arg is a steering angle in ADC units.
-      else if (op.equalsIgnoreCase("s"))
-      {
-        // Currently the motor runs at a fixed speed (stepper pulse rate).
-        steerTo(arg, 10);
-      }
-
-      // Fall-through case
-      else
-      {
-        Serial.print("Unknown command ");
-        Serial.println(op);
-      }
-
-      // Reset
-      command = "";
+      op = command.substring(0, spaceIndex);
+      arg = command.substring(spaceIndex + 1).toInt();
     }
+
+    // Please reply to request for latest steering angle reading
+    if (op.equalsIgnoreCase("u"))
+    {
+      String s = String("adc:") + (angleADC >> 4) + String(",steps:") + steps;
+      Serial.println(s);
+    }
+
+    // Steer the car - expecting that arg is a target position in ADC units.
+    else if (op.equalsIgnoreCase("s"))
+    {
+      // Currently the motor runs at a fixed speed (stepper pulse rate).
+      steerTo(arg, 10);
+    }
+
+    // Fall-through case
     else
     {
-      command += c;
+      Serial.print("Unknown command ");
+      Serial.println(op);
     }
+
+    // Reset
+    command = "";
+  }
+
+  else
+  {
+    command += c;
   }
 }
 
-// Current steering angle of front wheels [rad]
-// steerAngle = 1e-4 * map(angleADC >> 4, maxLeftADC, maxRightADC, leftSteerMax, rightSteerMax);
-
 void setup()
 {
-  pinMode(STEP_PIN, INPUT); // Don't enable steering motor yet
   pinMode(DIR_PIN, OUTPUT);
-  // pinMode(11, OUTPUT); // Outputs 250 Hz square wave if enabled
 
-  // Configure Timer 2 to generate a 500 Hz square wave on pin 3 to pulse the
-  // stepper motor driver. See comments in test_pwm.ino for details.
+  // Configure Timer 2 for phase-correct (count-up-count-down) PWM
+  // mode, with variable PWM frequency (controlled by OCR2A).
   TCCR2A = (1 << WGM20) | (1 << COM2A0) | (1 << COM2B1);
-  TCCR2B = (1 << CS22) | (1 << WGM22);
-  OCR2A = 220;
-  OCR2B = 110;
+  TCCR2B |= (1 << CS22);   // Prescale to F_CPU/64 (datasheet table 18-9)
+  TCCR2B |= (1 << WGM22);  // Toggle OC2A on compare match (table 18-4)
 
-  // Set rotation direction
-  digitalWrite(DIR_PIN, LOW);
+  // With the WGM22 and COM2A0 bits set, OC2A toggles based on the OCR2A value.
+  // Pin 11 frequency:
+  // f = 16MHz / 64 / (2*OCR2A) / 2 = 62500 Hz / OCR2A
+  // duty cycle = 50% (fixed)
+  OCR2A = 110;
 
   // Enable motor
   digitalWrite(ENABLE_PIN, LOW);
@@ -165,7 +141,17 @@ void setup()
 
 void loop()
 {
-  handleCommands();
+  if (Serial.available())
+  {
+    handleCommands();
+  }
 
   ewma(analogRead(POT_PIN), angleADC);
+}
+
+// Pin-change interrupt callback to increment step counter (only called when
+// PWM output is enabled).
+void count()
+{
+  steps++;
 }
