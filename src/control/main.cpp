88573c94323f9226c740d3d670bb10a8974179ed
Andrew Adare <andrewadare@gmail.com>
2016-08-25 21:17:06 -0600

    Switch to PWM interface to LidarLite sensor (less flaky)

--- src/control/main.cpp
+++ src/control/main.cpp
@@ -1,18 +1,15 @@
 #include "mbed.h"
 #include "BNO055.h"
-#include "LidarLitev2.h"
 #include "PIDControl.h"
 
-// Flag indicating whether the LidarLite sensor is hooked up
-const bool lidar_attached = true;
+// #define TUNE_PID 1 // Uncomment to use PIDTuner
+#ifdef TUNE_PID
+#include "PIDTuner.h"
+#endif
 
 // Sample interval in milliseconds
 const int timestep = 25;
 
-// Most recent lidar distance measurement [cm] and bearing [deg] with respect to
-// vehicle (0 degrees at front of car, increasing clockwise).
-int lidar_range = 0;
-float lidar_bearing = 0;
 
 // Smoothing parameter for exponentially-weighted moving average s[t] of
 // time series measurements y[t]:
@@ -46,47 +43,72 @@ PwmOut throttle_pwm(D6);
 BNO055 imu(I2C_SDA, I2C_SCL);
 
 // Driver class for Lidar Lite v2 rangefinder
-LidarLitev2 lidar(I2C_SDA, I2C_SCL);
+// LidarLitev2 lidar(I2C_SDA, I2C_SCL);
 
 // PID controller class for steering (eventually also throttle?)
 PIDControl pid(kp, ki, kd, initial_setpoint, timestep);
 
-// lidar_mode_pin idles high, dropping low when new data is ready to be read.
-DigitalIn lidar_mode_pin(D9);
+#ifdef TUNE_PID
+PIDTuner tuner(pid);
+#endif
+
+// Lidar sensor bearing angle [deg] with respect to vehicle centerline
+// (0 degrees at front of car, increasing clockwise).
+float lidar_bearing = 0;
+
+// Read LidarLite v2 sensor in PWM mode (I2C interface was flaky!)
+// Measured pulse width in microseconds = distance measurement in mm.
+InterruptIn lidar_pulse_pin(D9);
+volatile int lidar_pulse_start = 0; // us
+volatile int lidar_pulse_width = 0; // us
 
 // Variables for quadrature odometer encoder
 InterruptIn enc_a(D11);
 InterruptIn enc_b(D12);
 DigitalIn enc_b_in(D12);
-int32_t encoder_pos = 0;
-uint8_t prev_b = 0;
+volatile int32_t encoder_pos = 0;
+volatile uint8_t prev_b = 0;
 
 // Status indicator
 DigitalOut led(LED1);
 
 Timer timer;
 
-// Interrupt handler for quadrature decoder readout (channel a)
+// Interrupt handlers for channels A and B of quadrature rotary encoder
 void decode_a()
 {
   prev_b ? encoder_pos-- : encoder_pos++;
   led = !led;
 }
-
-// Interrupt handler for quadrature decoder readout (channel b)
 void decode_b()
 {
   prev_b = !prev_b;
+}
+
+// Interrupt handlers to measure PWM pulse width
+void on_lidar_pulse_rise()
+{
+  lidar_pulse_start = timer.read_us();
+}
+void on_lidar_pulse_fall()
+{
+  lidar_pulse_width = timer.read_us() - lidar_pulse_start;
 }
 
 void setup_imu()
 {
   pc.printf("Configuring IMU sensor\r\n");
   imu.reset();
+  int timeout_counter = 0;
   while (!imu.check())
   {
     led = !led;
     wait(0.1);
+    timeout_counter++;
+    if (timeout_counter > 100)
+    {
+      pc.printf("ERROR: Problem connecting to orientation sensor\r\n");
+    }
   }
   led.write(0);
   imu.setmode(OPERATION_MODE_NDOF);
@@ -115,71 +137,10 @@ void print()
             imu.quat.rawz,
             (int)(1000*pot),
             encoder_pos,
-            lidar_range,
+            lidar_pulse_width, // = range in mm
             lidar_bearing);
 
-  lidar_range = 0; // reset
-
   prev_time = now;
-}
-
-void handle_byte(char b)
-{
-  switch (b)
-  {
-  case 'p': // increase kp
-    kp += 0.01;
-    break;
-  case 'l': // decrease kp
-    kp -= 0.01;
-    break;
-  case 'i': // increase ki
-    ki += 1.0;
-    break;
-  case 'k': // decrease ki
-    ki -= 1.0;
-    break;
-  case 'd': // increase kd
-    kd += 0.001;
-    break;
-  case 'c': // decrease kd
-    kd -= 0.001;
-    break;
-
-  // Input completed - convert to a float in [0,1] and update setpoint
-  case '\r':
-  case '\n':
-    pid.setpoint = atof(cmd)/1000;
-    pid.clamp(pid.setpoint, pid.minOutput, pid.maxOutput);
-    pc.printf("\r\nsetpoint: %f\r\n", pid.setpoint);
-    cmd[0] = 0; // Reset for next use
-    break;
-
-  // Check if byte is a digit (0-9 = ASCII code point 48-57). If so, decode to
-  // the intended decimal integer value and append to the cmd string.
-  case 48:
-  case 49:
-  case 50:
-  case 51:
-  case 52:
-  case 53:
-  case 54:
-  case 55:
-  case 56:
-  case 57:
-    int digit = b - 48;
-    pc.printf("%d", digit);
-    sprintf(cmd, "%s%d", cmd, digit);
-    break;
-  default:
-  // Assume (without checking) input is one of p,l,i,k,d,c and update.
-    pid.setPID(kp, ki, kd);
-    pc.printf("kp,ki,kd: %f %f %f; p,i,d: %f %f %f; setpoint: %f; output: %f\r\n",
-              kp,ki,kd,
-              pid.kp,pid.ki,pid.kd,
-              pid.setpoint,
-              pid.output);
-  }
 }
 
 int main()
@@ -190,15 +151,6 @@ int main()
   pid.minOutput = 0.0;
   pid.maxOutput = 1.0;
   pid.setPID(kp, ki, kd);
-
-  if (lidar_attached)
-  {
-    // Configure LidarLite for continuous mode, which means that a new measurement
-    // will be available each time lidar_mode_pin drops low.
-    pc.printf("\r\nConfiguring lidar sensor\r\n");
-    lidar.configure();
-    lidar.beginContinuous();
-  }
 
   setup_imu();
 
@@ -216,6 +168,10 @@ int main()
   enc_b.rise(&decode_b);
   enc_b.fall(&decode_b);
 
+  // Attach interrupt handlers for LidarLite PWM (mode) pin.
+  lidar_pulse_pin.rise(&on_lidar_pulse_rise);
+  lidar_pulse_pin.fall(&on_lidar_pulse_fall);
+
   pc.printf("Beginning loop\r\n");
 
   while (true)
@@ -223,18 +179,24 @@ int main()
     pot = alpha*ain.read() + (1 - alpha)*pot;
 
     // pid.setpoint = atof(cmd)/1000; // TODO: read from master computer
+#ifdef TUNE_PID
+    if (pc.readable())
+    {
+      tuner.handleByte(pid, pc.getc());
+      if (strlen(tuner.message) > 0)
+      {
+        pc.printf(tuner.message);
+      }
+    }
+#endif
 
     pid.update(pot);
-    steer_pwm.write(pid.output);
 
-    if (lidar_attached && !lidar_mode_pin)
-    {
-      lidar_range = lidar.distanceContinuous();
-      lidar_bearing = 0; // TODO
-    }
+    steer_pwm.write(pid.output);
 
     imu.get_calib();
     imu.get_quat();
+
     print();
   }
 }
